27f2c38b0621f09b51c2d2c9e005da2325063ad2
dhanushika <dhanushika76@gmail.com>
2024-03-14 09:20:20 +0530

    update

--- application/modules/dashboard/views/home/home.php
+++ application/modules/dashboard/views/home/home.php
@@ -163,4 +163,22 @@
             icon: "info"
         });
     }
+
+    $(document).ready(function(){
+       
+       Swal.fire({
+         title: "<?php echo ($monthlytotalamount) ?> ",
+         showDenyButton: true,
+         showCancelButton: true,
+         confirmButtonText: "Save",
+         denyButtonText: `Don't save`
+       }).then((result) => {
+         /* Read more about isConfirmed, isDenied below */
+         if (result.isConfirmed) {
+           Swal.fire("Saved!", "", "success");
+         } else if (result.isDenied) {
+           Swal.fire("Changes are not saved", "", "info");
+         }
+       });
+   });
 </script>
